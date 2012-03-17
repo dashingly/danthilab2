c7d499e021a5a7c7e1f15eabf16fcc1af6b699b4
tmoreau.uoft@gmail.com <tmoreau.uoft@gmail.com@3d188af0-f4e5-c116-7bb7-292588c98de8>
2012-03-17 19:24:45 +0000

    Added files for lab3 (from lab2)

---
+++ trunk/mazewar_2/run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+
+# arguments to BrokerClient
+# $1 = hostname of where Mazewar is located
+# $2 = port # where Mazewar is listening
+
+java Mazewar $1 $2
---
+++ trunk/mazewar_2/server.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# arguments to MazeServer
+# $1 = port # where MazeServer is listening
+# $2 = # of clients that are playing the game
+
+java MazeServer $1 $2
+
